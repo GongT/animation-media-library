1e0e884d2dbae94aa13121bee366b83d9530896e
GongT <admin@gongt.me>
2025-07-28 22:50:50 +0800

    bgmtv-api v0.0.2

--- @packages/bangumi-client/src/test.ts
+++
@@ -1,4 +0,0 @@
-import { BangumiClient } from './bangumi-client.js';
-
-const bgm = new BangumiClient('HrIrcPCKpzGC8CtXEBvUNXyvo97vJORYHbFZQejd', {});
-console.log(bgm);
--- internal/scripts/src/commands/pre-publish.ts
+++ internal/scripts/src/commands/pre-publish.ts
@@ -1,15 +1,16 @@
-import { cpSync, existsSync, writeFileSync } from 'node:fs';
-import { resolve } from 'node:path';
-import { projectRoot } from '../tools/paths.js';
+import { loadJsonFile, writeJsonFileBack } from "@idlebox/json-edit";
+import { cpSync, existsSync, writeFileSync } from "node:fs";
+import { resolve } from "node:path";
+import { projectRoot } from "../tools/paths.js";
 
-console.log('pre-publish script running...');
-console.log('cwd:', process.cwd());
+console.log("pre-publish script running...");
+console.log("cwd:", process.cwd());
 
 function writeFile(filePath: string, content: string) {
 	if (existsSync(filePath)) {
 		throw new Error(`文件不应存在: ${filePath}`);
 	}
-	writeFileSync(filePath, content, 'utf-8');
+	writeFileSync(filePath, content, "utf-8");
 }
 
 const npmrcContent = `
@@ -18,15 +19,27 @@ git-checks=false
 access=public
 `;
 
-writeFile('.npmrc', npmrcContent);
+writeFile(".npmrc", npmrcContent);
 
 const npmignoreContent = `
+**/.*
 **/test/
 config/
 temp/
 *.tgz
 `;
 
-writeFile('.npmignore', npmignoreContent);
+writeFile(".npmignore", npmignoreContent);
 
-cpSync(resolve(projectRoot, 'LICENSE'), 'LICENSE');
+cpSync(resolve(projectRoot, "LICENSE"), "LICENSE");
+
+const pkgJson = await loadJsonFile("package.json");
+delete pkgJson.publishConfig;
+
+for (const [name, version] of Object.entries(pkgJson.devDependencies ?? {})) {
+	if (name.startsWith("@internal/") || version === "") {
+		delete pkgJson.devDependencies[name];
+	}
+}
+
+writeJsonFileBack(pkgJson);
